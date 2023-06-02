ba70c9f16565dc7b8f7eff31e8b0d2f2051df0a8
prakashojha <prakashojha2005@gmail.com>
2023-06-02 02:25:31 +0000

    See https://github.com/prakashojha/DesignSystem/commit/fe34d5fcf9730b829035027131ab1140bab389be from refs/heads/main

--- Sources/DS-iOS/Styles/ColorExtension.swift
+++ Sources/DS-iOS/Styles/ColorExtension.swift
@@ -16,7 +16,7 @@ public extension Color {
         public let colorbuttonboxshadowDefault = Color(.sRGB, red: 255/255, green: 194/255, blue: 14/255, opacity: 1) 
 
         // background
-        public let colorbuttonbackgroundPrimary = Color(.sRGB, red: 255/255, green: 0/255, blue: 0/255, opacity: 1) 
+        public let colorbuttonbackgroundPrimary = Color(.sRGB, red: 0/255, green: 255/255, blue: 0/255, opacity: 1) 
         public let colorbuttonbackgroundTertiary = Color(.sRGB, red: 255/255, green: 255/255, blue: 255/255, opacity: 1) 
         public let colorbuttonbackgroundSecondary = Color(.sRGB, red: 255/255, green: 255/255, blue: 255/255, opacity: 1) 
     }
